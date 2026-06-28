cd32d346e809bb285af090640df1c9f7d088fca0
amir <a.m.asadi2001@gmail.com>
2026-06-28 13:20:15 +0330

    API:create dbcontext

---
+++ API/Blog.API/Blog.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Blog.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.API.Data;
+
+public class AppDbContext : DbContext
+{
+  public AppDbContext(DbContextOptions options) : base(options)
+  {
+  }
+
+  private DbSet<BlogPost> BlogPosts { get; set; }
+  private DbSet<Category> Categories { get; set; }
+}
