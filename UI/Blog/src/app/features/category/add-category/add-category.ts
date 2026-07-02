ec3b6a2d11f44c91bf63ef9415cc955f9c3b46da
amir <a.m.asadi2001@gmail.com>
2026-07-02 15:06:59 +0330

    UI:add simple validation for add category form

--- UI/Blog/src/app/features/category/add-category/add-category.ts
+++ UI/Blog/src/app/features/category/add-category/add-category.ts
@@ -8,13 +8,47 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './add-category.css',
 })
 export class AddCategory {
-  form = new FormGroup({
-    title: new FormControl(''),
-    url: new FormControl(''),
+  addCategoryFormGroup = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(100),
+    ]),
+    url: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(100),
+    ]),
   });
 
+  get nameFormControl() {
+    return this.addCategoryFormGroup.controls.title;
+  }
+  get urlFormControl() {
+    return this.addCategoryFormGroup.controls.url;
+  }
+
+  showError(name: FormControl): string {
+    if (name.invalid && (name.dirty || name.touched)) {
+      if (name.hasError('required')) {
+        return `این فیلد اجباری است.`;
+      }
+      if (name.hasError('minlength')) {
+        return `حداقل ۲ کاراکتر `;
+      }
+      if (name.hasError('maxlength')) {
+        return `حداکثر ۱۰۰ کاراکتر`;
+      }
+    }
+    return '';
+  }
+
   onSubmit() {
-    console.log(this.form.value);
-    this.form.reset();
+    if (this.addCategoryFormGroup.valid) {
+      console.log(this.addCategoryFormGroup.value);
+      this.addCategoryFormGroup.reset();
+    } else {
+      console.log('Err');
+    }
   }
 }
