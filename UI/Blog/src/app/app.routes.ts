9c3a2a2caa0a9b85d8bddcff301fdd0684346f20
amir <a.m.asadi2001@gmail.com>
2026-07-06 10:31:56 +0330

    UI:create blogpost list component and add it to navbar

--- UI/Blog/src/app/app.routes.ts
+++ UI/Blog/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CategoryList } from './features/category/category-list/category-list';
 import { AddCategory } from './features/category/add-category/add-category';
 import { EditCategory } from './features/category/edit-category/edit-category';
+import { BlogpostList } from './features/blogpost/blogpost-list/blogpost-list';
 
 export const routes: Routes = [
   { path: 'admin/categories', component: CategoryList },
   { path: 'admin/category/add', component: AddCategory },
   { path: 'admin/categories/edit/:id', component: EditCategory },
+  { path: 'admin/blogposts', component: BlogpostList },
 ];
